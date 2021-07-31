dade948a58596901c931e9cbad4cb10b261f60b1
ms77grz <ms77grz@gmail.com>
2021-07-31 23:33:30 +0300

    Simple Counter

--- src/components/UseStateCounter.js
+++ src/components/UseStateCounter.js
@@ -9,16 +9,16 @@ export default function UseStateCounter() {
       <section style={{ margin: '4rem 0' }}>
         <h2>regular counter</h2>
         <h1>{value}</h1>
+        <button className='btn' onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className='btn' onClick={reset}>
+          reset
+        </button>
+        <button className='btn' onClick={() => setValue(value + 1)}>
+          increase
+        </button>
       </section>
-      <button className='btn' onClick={() => setValue(value - 1)}>
-        decrease
-      </button>
-      <button className='btn' onClick={reset}>
-        reset
-      </button>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        increase
-      </button>
     </>
   );
 }
