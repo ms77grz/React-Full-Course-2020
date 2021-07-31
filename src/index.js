843b42eae6132827538bad919b8c32f9245f7040
ms77grz <ms77grz@gmail.com>
2021-07-31 16:23:30 +0300

    Key Prop And Spread Operator

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import './index.css';
 // setup vars
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/81Zj4ibm%2BhL._AC_UL200_SR200,200_.jpg',
     title: 'How to Catch a Mermaid',
     author: 'Adam Wallace',
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/91hzcTMy3GL._AC_UL200_SR200,200_.jpg',
     title: 'Grumpy Monkey',
     author: 'Suzanne Lang',
   },
   {
+    id: 3,
     img: 'https://images-na.ssl-images-amazon.com/images/I/81vTRhCo4sL._AC_UL200_SR200,200_.jpg',
     title: 'The Invisible String',
     author: 'Patrice Karst',
@@ -27,14 +30,14 @@ function BookList() {
   return (
     <section className='booklist'>
       {books.map(book => (
-        <Book book={book} />
+        <Book key={book.id} {...book} />
       ))}
     </section>
   );
 }
 
-const Book = props => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className='book'>
       <img src={img} alt='' />
