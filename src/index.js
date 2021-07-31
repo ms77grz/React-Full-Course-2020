85af7106102f9bc9f451c4d03450aca1da399fd9
ms77grz <ms77grz@gmail.com>
2021-07-31 14:27:58 +0300

    JSX JavaScript

--- src/index.js
+++ src/index.js
@@ -8,35 +8,25 @@ function BookList() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
+const author = 'Adam Wallace';
+
 const Book = () => {
+  const title = 'How to Catch a Mermaid';
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src='https://images-na.ssl-images-amazon.com/images/I/81Zj4ibm%2BhL._AC_UL200_SR200,200_.jpg'
+        alt=''
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      {/* this is a comment in JSX code */}
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/81Zj4ibm%2BhL._AC_UL200_SR200,200_.jpg'
-    alt=''
-  />
-);
-
-const Title = () => <h1>How to Catch a Mermaid</h1>;
-
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-    Adam Wallace
-  </h4>
-);
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
