e7ed303b903899909340a070ace1d42ffbcac9c0
ms77grz <ms77grz@gmail.com>
2021-07-31 20:30:49 +0300

    Import And Export Statements

--- src/index.js
+++ src/index.js
@@ -4,65 +4,22 @@ import ReactDOM from 'react-dom';
 // CSS
 import './index.css';
 
-// setup vars
-const books = [
-  {
-    id: 1,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81Zj4ibm%2BhL._AC_UL200_SR200,200_.jpg',
-    title: 'How to Catch a Mermaid',
-    author: 'Adam Wallace',
-  },
-  {
-    id: 2,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91hzcTMy3GL._AC_UL200_SR200,200_.jpg',
-    title: 'Grumpy Monkey',
-    author: 'Suzanne Lang',
-  },
-  {
-    id: 3,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81vTRhCo4sL._AC_UL200_SR200,200_.jpg',
-    title: 'The Invisible String',
-    author: 'Patrice Karst',
-  },
-];
+// named import
+import { data } from './books';
+import { greeting } from './testing/testing';
+
+// in default export the naming of import is completely independent and we can use any name we like
+import SpesificBook from './Book';
 
 function BookList() {
+  console.log(greeting);
   return (
     <section className='booklist'>
-      {books.map(book => (
-        <Book key={book.id} {...book} />
+      {data.map(book => (
+        <SpesificBook key={book.id} {...book} />
       ))}
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  const clickHandler = e => {
-    console.log(e);
-    console.log(e.target);
-    alert('hello world');
-  };
-  const complexExample = author => {
-    console.log(author);
-  };
-  return (
-    <article
-      className='book'
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt='' />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type='button' onClick={clickHandler}>
-        reference example
-      </button>
-      <button type='button' onClick={() => complexExample(author)}>
-        more complex example
-      </button>
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
