b3a89f32361e303c15647ac3cedf2147648a4a29
ms77grz <ms77grz@gmail.com>
2021-07-31 23:05:10 +0300

    The useState - Multiple State Values

--- src/components/UseStateObject.js
+++ src/components/UseStateObject.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
 
 export default function UseStateObject() {
-  const [people, setPeople] = useState({
-    name: 'peter',
-    age: 24,
-    message: 'random message',
-  });
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('random message');
 
   const changeMessage = () => {
-    setPeople({ ...people, message: 'hello world' });
+    setMessage('hello world');
   };
+
   return (
     <>
-      <h3>{people.name}</h3>
-      <h3>{people.age}</h3>
-      <h3>{people.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className='btn' onClick={changeMessage}>
         change message
       </button>
