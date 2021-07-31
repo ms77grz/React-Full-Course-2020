6bddb572b4e42a7d7f0757fc3e0e2100145fa35b
ms77grz <ms77grz@gmail.com>
2021-07-31 12:39:57 +0300

    First Component in Detail

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Greeting() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
 }
+
+// function Greeting() {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'Hello, React!')
+//   );
+// }
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
