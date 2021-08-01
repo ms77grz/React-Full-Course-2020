20bedf80ab715ec4a297e790a60b7956ff8f4d14
ms77grz <ms77grz@gmail.com>
2021-08-01 22:45:07 +0300

    Ternary Operator

--- src/components/ShortCircuit.js
+++ src/components/ShortCircuit.js
@@ -1,7 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function ShortCircuit() {
   const [text, setText] = useState('');
+  const [how, setHow] = useState(false);
+
+  const whatNext = () => {
+    if (text !== 'Learn JS') {
+      setText('Learn JS');
+    } else {
+      setText('');
+    }
+  };
 
   return (
     <>
@@ -13,9 +22,18 @@ export default function ShortCircuit() {
           ? 'To Be A Frontend Web Developer'
           : 'To Be A Professional Web Designer'}
       </h3>
-      <button className='btn' onClick={() => setText('Learn JS')}>
+
+      <button className='btn' onClick={whatNext}>
         {text ? 'before' : 'next'}
       </button>
+      <button className='btn' onClick={() => setHow(!how)}>
+        want to know how?
+      </button>
+      {how && (
+        <h2 className='item'>
+          <a href='https://google.com'>Google It!</a>
+        </h2>
+      )}
     </>
   );
 }
