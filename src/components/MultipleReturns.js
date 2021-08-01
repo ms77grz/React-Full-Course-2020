7da8ec03212bda0db1881c637819c928bc622d43
ms77grz <ms77grz@gmail.com>
2021-08-01 18:55:32 +0300

    Multiple Returns - Fetching Data

--- src/components/MultipleReturns.js
+++ src/components/MultipleReturns.js
@@ -1,13 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users/ms77grz';
 
 export default function UseEffectFetchData() {
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
 
-  if (loading) {
-    return <h2>Loading...</h2>;
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then(user => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
 
-  return <h2>multiple returns</h2>;
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 }
